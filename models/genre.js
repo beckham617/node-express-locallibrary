39a7ba9b85309a44010db40d9e02670eddebf91b
liangli <liangli5100@gmail.com>
2020-10-10 19:47:24 -0700

    First commit

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, min:3, max: 100}
+  }
+);
+
+// Virtual for bookinstance's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
